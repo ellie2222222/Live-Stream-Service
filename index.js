01baf4cac09503c69c769fb9a4ad4a8e02bb0295
Lê Quý Hoàng Tâm <127521514+ellie2222222@users.noreply.github.com>
2024-09-13 10:30:20 +0700

    configure cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.DEVELOPMENT_PORT],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
