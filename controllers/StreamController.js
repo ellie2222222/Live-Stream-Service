847028957f529d54eb8f34e96b1e407b93f58c1e
khanhtung <ktung7574@gmail.com>
2024-09-19 16:26:51 +0700

    meo

--- controllers/StreamController.js
+++ controllers/StreamController.js
@@ -14,6 +14,186 @@ const BUNNY_CDN_URL = "https://sg.storage.bunnycdn.com/live-stream-service/";
 const BUNNY_CDN_API_KEY = "e68740b8-e7b2-4df2-82b616b8ab35-77e2-42d6";
 const axios = require("axios");
 
+const uploadToBunnyCDN = async (filePath, fileName, userFolder) => {
+  const readStream = fs.createReadStream(filePath);
+  const storageZone = process.env.BUNNYCDN_STORAGE_ZONE_NAME;
+
+  const options = {
+    method: "PUT",
+    host: "storage.bunnycdn.com",
+    path: `/${storageZone}/video/${userFolder}/${fileName}`,
+    headers: {
+      AccessKey: process.env.BUNNYCDN_STORAGE_PASSWORD,
+      "Content-Type": "application/octet-stream",
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate", // Disable caching
+      Expires: "0",
+      Pragma: "no-cache",
+    },
+  };
+
+  const req = https.request(options, (res) => {
+    res.on("data", (chunk) => {
+      console.log(chunk.toString("utf8"));
+    });
+  });
+
+  req.on("error", (error) => {
+    console.error(error);
+  });
+
+  readStream.pipe(req);
+};
+
+const deleteFromBunnyCDN = async (userFolder, fileName) => {
+  const storageZone = process.env.BUNNYCDN_STORAGE_ZONE_NAME;
+
+  const options = {
+    method: "DELETE",
+    host: "storage.bunnycdn.com",
+    path: `/${storageZone}/video/${userFolder}/${fileName}`,
+    headers: {
+      AccessKey: process.env.BUNNYCDN_STORAGE_PASSWORD,
+    },
+  };
+  console.log(options.path);
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      let responseBody = "";
+      res.on("data", (chunk) => {
+        responseBody += chunk.toString();
+      });
+      res.on("end", () => {
+        if (res.statusCode === 404) {
+          resolve("Folder not found");
+        } else if (res.statusCode === 200) {
+          console.log(`Deleted folder ${userFolder}: ${responseBody}`);
+          resolve(responseBody);
+        } else {
+          reject(
+            new Error(
+              `Failed to delete folder ${userFolder}: ${res.statusCode}`
+            )
+          );
+        }
+      });
+    });
+
+    req.on("error", (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
+};
+
+const purgeBunnyCDNCache = async () => {
+  const options = {
+    method: "POST",
+    host: "api.bunny.net",
+    path: `/pullzone/${process.env.BUNNYCDN_PULLZONE_ID}/purgeCache`,
+    headers: {
+      AccessKey: process.env.BUNNYCDN_API_KEY,
+    },
+  };
+
+  const req = https.request(options, (res) => {
+    res.on("data", (chunk) => {
+      console.log(chunk.toString("utf8"));
+    });
+  });
+
+  req.on("error", (error) => {
+    console.error(error);
+  });
+
+  req.end();
+};
+
+// Function to replace .ts file paths in .m3u8 with BunnyCDN URLs
+const replaceTsWithCDN = (m3u8FilePath, cdnUrl, mainFileName, userFolder) => {
+  let m3u8Content = fs.readFileSync(m3u8FilePath, "utf8");
+  m3u8Content = m3u8Content.replace(
+    new RegExp(`${mainFileName}-${userFolder}-segment-\\d{3}\\.ts`, "g"),
+    (match) => {
+      return `${cdnUrl}${match}`;
+    }
+  );
+  fs.writeFileSync(m3u8FilePath, m3u8Content);
+};
+
+// Function to upload .ts segment files
+async function uploadTsFiles(mainFileName, userFolder) {
+  const files = fs.readdirSync(outputDir);
+  for (const file of files) {
+    if (
+      file.endsWith(".ts") &&
+      file.includes(mainFileName) &&
+      file.includes(userFolder)
+    ) {
+      const filePath = path.join(outputDir, file);
+      const fileName = path.basename(file);
+      await uploadToBunnyCDN(filePath, fileName, userFolder);
+    }
+  }
+}
+
+let inputURL = "rtmp://localhost:1935/live";
+
+// Function to save the stream using FFmpeg
+const saveStreamToBunny = async (userFolder) => {
+  const mainFileName = "stream-result";
+  const outputFilename = `${outputDir}/${mainFileName}.m3u8`;
+  const hostName =
+    process.env.BUNNYCDN_HOSTNAME || "live-stream-platform.b-cdn.net";
+  try {
+    const connection = new DatabaseTransaction();
+    const email = await connection.userRepository.findUserByEmail(userFolder);
+    if (!email) {
+      throw new Error("Invalid key");
+    }
+
+    inputStream = `${inputURL}/${userFolder}`;
+
+    ffmpeg(inputStream)
+      .inputFormat("flv")
+      .outputOptions([
+        "-hls_time 15",
+        "-hls_list_size 0",
+        "-hls_flags delete_segments",
+        `-hls_segment_filename ${outputDir}/${mainFileName}-${userFolder}-segment-%03d.ts`,
+        "-t 30",
+        "-f hls",
+      ])
+      .output(outputFilename)
+      .on("end", async () => {
+        replaceTsWithCDN(
+          outputFilename,
+          `https://${hostName}/video/${userFolder}/`,
+          mainFileName,
+          userFolder
+        );
+        await deleteFromBunnyCDN(
+          userFolder,
+          `stream-result-${userFolder}.m3u8`
+        );
+        await uploadToBunnyCDN(
+          outputFilename,
+          `stream-result-${userFolder}.m3u8`,
+          userFolder
+        );
+        await uploadTsFiles(mainFileName, userFolder);
+        await purgeBunnyCDNCache();
+      })
+      .on("error", (err) => {
+        console.error("Error:", err);
+      })
+      .run();
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
 class StreamController {
   async getUrlStream(req, res) {
     try {
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const http = require("http");
-const axios = require("axios");
-const socketIo = require("socket.io");
-const { spawn } = require("child_process");
-const fs = require("fs");
-const { Readable } = require("stream");
 const cors = require("cors");
+const http = require("http");
+const NodeMediaServer = require("node-media-server");
+const socketIo = require("socket.io");
+const { saveStreamToBunny } = require("./services/StreamService");
+const { createAMessageService } = require("./services/MessageService");
+const { findUser } = require("./services/UserService");
 
 // Initialize application and server
 const app = express();
@@ -17,7 +17,6 @@ const io = socketIo(server, {
     methods: ["GET", "POST"],
   },
 });
-let viewersCount = 0;
 // Middleware
 app.use(
   cors({
@@ -28,6 +27,60 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  // Keep track of the rooms this socket is in
+  const userRooms = new Set();
+
+  socket.on("join_room", (roomId) => {
+    socket.join(roomId);
+    userRooms.add(roomId);
+    console.log(`User joined room: ${roomId}`);
+    updateViewersCount(roomId);
+  });
+
+  socket.on("leave_room", (roomId) => {
+    handleLeaveRoom(socket, roomId);
+    userRooms.delete(roomId);
+  });
+
+  socket.on("send_message", async (data) => {
+    const { roomId, message, userId } = data;
+    console.log(data);
+    await createAMessageService(userId, roomId, message.text);
+    const user = await findUser(userId);
+    io.to(roomId).emit("new_message", {
+      sender: user.username,
+      text: message.text,
+    });
+  });
+
+  // Handle disconnecting (fires before disconnect)
+  socket.on("disconnecting", () => {
+    console.log("User disconnecting");
+    for (const room of userRooms) {
+      handleLeaveRoom(socket, room);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    // Any additional cleanup if needed
+  });
+});
+
+function handleLeaveRoom(socket, roomId) {
+  socket.leave(roomId);
+  console.log(`User left room: ${roomId}`);
+  updateViewersCount(roomId);
+}
+
+function updateViewersCount(roomId) {
+  const viewersCount = io.sockets.adapter.rooms.get(roomId)?.size || 0;
+  io.to(roomId).emit("viewers_count", viewersCount);
+}
 
 // Import routes
 const authRoutes = require("./routes.js/AuthRoute");
@@ -47,143 +100,39 @@ app.use((req, res, next) => {
   next();
 });
 
-let videoBuffer = Buffer.alloc(0);
-let videoId = null;
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 60,
+    ping_timeout: 30,
+    allow_origin: "*",
+  },
+  http: {
+    port: 8000,
+    mediaroot: "./output",
+    allow_origin: "*",
+  },
+};
 
-// Function to create a video entry if it doesn't exist
-async function createVideoEntry() {
-  if (!videoId) {
-    const libraryId = process.env.BUNNYCDN_VIDEO_LIBRARY_ID || "307543";
-    const accessKey =
-      process.env.BUNNYCDN_ACCESS_KEY ||
-      "679c0c32-01dc-4fa5-87a4c3d7330d-e7f1-4aea";
+const nms = new NodeMediaServer(config);
 
-    try {
-      const createVideo = await axios.post(
-        `https://video.bunnycdn.com/library/${libraryId}/videos`,
-        { title: "livestream nè 1" },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            AccessKey: accessKey,
-          },
-        }
-      );
-      videoId = createVideo.data.guid;
-      console.log(`Video created with ID: ${videoId}`);
-    } catch (error) {
-      console.error("Error creating video entry:", error);
-    }
+// Handle the 'postPublish' event to start saving the stream once it's live
+nms.on("postPublish", async (_, streamPath, _params) => {
+  const streamKey = streamPath.split("/").pop();
+
+  try {
+    await saveStreamToBunny(streamKey);
+  } catch (error) {
+    console.error(
+      `Failed to save stream for key '${streamKey}': ${error.message}`
+    );
   }
-}
-
-io.on("connection", (socket) => {
-  viewersCount++;
-  io.emit("views", viewersCount);
-  console.log("A user connected");
-
-  socket.on("video_chunk", (chunk) => {
-    videoBuffer = Buffer.concat([videoBuffer, Buffer.from(chunk)]);
-  });
-
-  socket.on("new_comment", (comment) => {
-    // Broadcast the comment to all clients except the sender
-    socket.broadcast.emit("new_comment", {
-      ...comment,
-      sender: "Other User", // You can replace this with actual user identification if available
-    });
-  });
-  socket.on("disconnect", async () => {
-    viewersCount--;
-    io.emit("views", viewersCount); // Send updated count to all clients
-    console.log("User disconnected");
-
-    // Save buffer to file
-    fs.writeFileSync("video_input.mp4", videoBuffer);
-
-    // Convert video to HLS
-    const ffmpeg = spawn("ffmpeg", [
-      "-i",
-      "video_input.mp4",
-      "-c:v",
-      "libx264",
-      "-vf",
-      "fps=30",
-      "-hls_time",
-      "10",
-      "-hls_playlist_type",
-      "event",
-      "-hls_flags",
-      "delete_segments+append_list",
-      "-b:v",
-      "2M",
-      "-maxrate",
-      "2M",
-      "-bufsize",
-      "4M",
-      "output.m3u8",
-    ]);
-
-    ffmpeg.on("close", async (code) => {
-      console.log("FFmpeg exited with code", code);
-      if (code === 0) {
-        fs.readdir("./", async (err, files) => {
-          if (err) {
-            console.error("Error reading directory:", err);
-            return;
-          }
-
-          // Filter and upload .ts files
-          const tsFiles = files.filter((file) => file.endsWith(".ts"));
-          if (tsFiles.length === 0) {
-            console.log("No .ts files found.");
-            return;
-          }
-
-          await createVideoEntry();
-
-          tsFiles.forEach(async (file) => {
-            fs.readFile(file, async (err, data) => {
-              if (err) {
-                console.error("Error reading .ts file:", err);
-                return;
-              }
-
-              const libraryId =
-                process.env.BUNNYCDN_VIDEO_LIBRARY_ID || "307543";
-              const accessKey =
-                process.env.BUNNYCDN_ACCESS_KEY ||
-                "679c0c32-01dc-4fa5-87a4c3d7330d-e7f1-4aea";
-
-              try {
-                // Upload the .ts file to the existing video entry
-                await axios.put(
-                  `https://video.bunnycdn.com/library/${libraryId}/videos/${videoId}/`,
-                  data,
-                  {
-                    headers: {
-                      "Content-Type": "video/mp2t",
-                      AccessKey: accessKey,
-                      "Content-Length": data.length,
-                    },
-                  }
-                );
-
-                console.log(`Uploaded ${file} successfully`);
-              } catch (error) {
-                console.error("Error during upload:", error);
-              }
-            });
-          });
-        });
-      }
-    });
-
-    ffmpeg.stderr.on("data", (data) => {
-      console.error("FFmpeg stderr:", data.toString());
-    });
-  });
 });
+
+// Start the media server
+nms.run();
 
 // Start server
 const port = process.env.DEVELOPMENT_PORT || 4000;
