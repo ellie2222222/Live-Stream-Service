8f532df076c5b899267285ba5cb6db502ae52268
Nguyen Khanh Tung <ktung7574@gmail.com>
2024-09-24 09:55:10 +0700

    merge

--- middlewares/typesMapping.js
+++ middlewares/typesMapping.js
@@ -1,11 +1,10 @@
-
 const typesMapping = {
-    0: "Chưa chọn",
-    1: "LOL",
-    2: "VALORANT",
-    3: "PUBG",
-    4: "CSGO",
-    5: "ĐỘT KÍCH",
+  0: "Chưa chọn",
+  1: "LOL",
+  2: "VALORANT",
+  3: "PUBG",
+  4: "CSGO",
+  5: "ĐỘT KÍCH",
 };
 
 export default typesMapping;
